3535d2146a3368d9c4110f6ce9eeb09393c19c58
liumingliang <liumingliang@xueqiu.com>
2020-02-03 00:30:23 +0800

    支持xml配置

--- mybatis-plus-sample-quickstart/src/main/java/com/baomidou/mybatisplus/samples/quickstart/QuickstartApplication.java
+++ mybatis-plus-sample-quickstart/src/main/java/com/baomidou/mybatisplus/samples/quickstart/QuickstartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+//@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class QuickstartApplication {
 
     public static void main(String[] args) {
--- mybatis-plus-sample-quickstart/src/test/java/com/baomidou/mybatisplus/samples/quickstart/SampleTest.java
+++ mybatis-plus-sample-quickstart/src/test/java/com/baomidou/mybatisplus/samples/quickstart/SampleTest.java
@@ -1,5 +1,7 @@
 package com.baomidou.mybatisplus.samples.quickstart;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 import com.baomidou.mybatisplus.samples.quickstart.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -21,11 +23,23 @@ public class SampleTest {
     @Resource
     private UserMapper userMapper;
 
-    @Test
+    //@Test
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
         userList.forEach(System.out::println);
+    }
+    @Test
+    public void testSelect1() {
+        System.out.println(("----- selectAll method test ------"));
+        Page<User> pageUser = userMapper.selectPage(new Page<User>(1, 10),new QueryWrapper<User>().orderByAsc("name").like("name","Tom"));
+        pageUser.getRecords().forEach(System.out::println);
+    }
+    @Test
+    public void test() {
+        System.out.println(("----- selectAll method test ------"));
+        User user = userMapper.selectByUsername("Jone");
+        System.out.println(user);
     }
 }
